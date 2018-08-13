257301c512c48b2a3c90b53cbfcba4c17e918801
Arnau Sanchez <pyarnau@gmail.com>
2018-08-13 11:57:33 +0200

    Update commands spec

--- src/__tests__/commands.spec.js
+++ src/__tests__/commands.spec.js
@@ -33,7 +33,6 @@ const users = {
   kamara: {
     username: "kamara",
     id: "N3PZBUlN8vq",
-    email: "johnkamara@hmail.com",
   },
 };
 
@@ -79,7 +78,7 @@ describe("commands", () => {
 
     it("sends emails to subscribers on interpretation created", () => {
       expect(helpers.sendEmail).toBeCalledWith(expect.any(Object), expect.objectContaining({
-        "recipients": [users.kamara.email],
+        "recipients": expect.arrayContaining([expect.stringContaining(users.kamara.username)]),
         "subject": "John Traore created an interpretation",
         "text": expect.stringContaining(`/dhis-web-visualizer/index.html?id=R9A0rvAydpn&interpretationid=${interpretations[0].id}`),
       }));
@@ -87,7 +86,7 @@ describe("commands", () => {
 
     it("sends emails to subscribers on comment created", () => {
       expect(helpers.sendEmail).toBeCalledWith(expect.any(Object), expect.objectContaining({
-        "recipients": [users.kamara.email],
+        "recipients": expect.arrayContaining([expect.stringContaining(users.kamara.username)]),
         "subject": "John Traore created an interpretation comment",
         "text": expect.stringContaining(`/dhis-web-visualizer/index.html?id=R9A0rvAydpn&interpretationid=${interpretations[0].id}`),
       }));
@@ -121,7 +120,7 @@ describe("commands", () => {
 
     it("sends newsletter to subscribers", () => {
       expect(helpers.sendEmail).toBeCalledWith(expect.any(Object), expect.objectContaining({
-        "recipients": [users.kamara.email],
+        "recipients": expect.arrayContaining([expect.stringContaining(users.kamara.username)]),
         "subject": "DHIS2 Interpretations Digest (08/13/2018)",
       }));
     });
